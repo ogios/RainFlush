534249ea4d527f14beac2a86b67f38bf2a5eff76
ogios <2134692955@qq.com>
2022-12-12 21:52:11 +0800

    HNDX

---
+++ hb/main.py
@@ -0,0 +1,22 @@
+import USER
+import RUN_TMP
+import click
+import os
+
+
+@click.command()
+@click.option("-u", type=str, default="T104755220086", help="学号")
+def main(u):
+    print(f"当前pid: {os.getpid()}")
+    us = USER.User(u)
+    cookie = us.getCookie()
+    run = RUN_TMP.run(cookie)
+    if run.getCourses():
+        run.chooseCourse()
+        if run.getVideo():
+            if run.getUndone():
+                run.flushALL()
+
+
+if __name__ == "__main__":
+    main()
